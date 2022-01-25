4a6c78af653e441b970f7c44686e056e36412f55
ozonar <7EvilCyberBears>
2022-01-26 03:59:09 +0700

    Remove yandex api

---
+++ www/config.dist.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www/zdnizbot/');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '');
+define('VK_API_ACCESS_TOKEN', '');
